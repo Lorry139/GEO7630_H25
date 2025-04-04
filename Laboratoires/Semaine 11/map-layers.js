67162cc7e5e66d9d7660bdc9f43019a3d099d7d8
Lorry139 <randrianirina.lorry_mael@courrier.uqam.ca>
2025-04-04 01:21:21 +0000

    Update

--- Laboratoires/Semaine 11/map-layers.js
+++ Laboratoires/Semaine 11/map-layers.js
@@ -1,6 +1,6 @@
 var commercesSource = {
   type: 'geojson',
-  data: 'https://donnees.montreal.ca/dataset/.../download/businesses.geojson'
+  data: 'https://donnees.montreal.ca/dataset/c1d65779-d3cb-44e8-af0a-b9f2c5f7766d/resource/ece728c7-6f2d-4a51-a36d-21cd70e0ddc7/download/businesses.geojson'
 };
 
 var commercesLayer = {
@@ -32,4 +32,38 @@ var commercesLayer = {
     'circle-stroke-width': 1
   },
   filter: ['==', ['get', 'statut'], 'Ouvert']
+};
+
+var arrondissementsSource = {
+  type: 'geojson',
+  data: 'https://donnees.montreal.ca/dataset/9797a946-9da8-41ec-8815-f6b276dec7e9/resource/e18bfd07-edc8-4ce8-8a5a-3b617662a794/download/limites-administratives-agglomeration.geojson'
+};
+ 
+var arrondissementsLayer = {
+  id: 'arrondissements',
+  type: 'fill',
+  source: 'arrondissements_source',
+  paint: {
+    'fill-color': '#ccc',
+    'fill-opacity': 0.3,
+    'fill-outline-color': '#000'
+  }
+};
+
+var arrondissementsLabelsLayer = {
+  id: 'arrondissements-labels',
+  type: 'symbol',
+  source: 'arrondissements_source', // ✅ correspond bien au nom de la source
+  layout: {
+    'text-field': ['get', 'NOM'],
+    'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+    'text-size': 14,
+    'text-anchor': 'center'
+  },
+  paint: {
+    'text-color': '#111',
+    'text-halo-color': '#fff',
+    'text-halo-width': 1.5
+  },
+
 };
