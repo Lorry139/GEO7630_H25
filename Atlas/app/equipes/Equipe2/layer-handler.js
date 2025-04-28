41bd225ac49a363bb744f0001561e9d561b2c62c
Lorry139 <randrianirina.lorry_mael@courrier.uqam.ca>
2025-04-28 14:14:07 +0000

    Up

---
+++ Atlas/app/equipes/Equipe2/layer-handler.js
@@ -0,0 +1,27 @@
+// Cette fonction supprime toutes les couches et sources de la carte
+ 
+// Ajouter ici tous les identifiants unique (id) de layers (ceux de map.AddLayers())
+ 
+var myLayers = ['Denssité_hexagon-3d','Nbre_de_site','Denssité_hexagon','Arret_Stationnement', 'Nbre_de_site-3d','buffer-arret-layer','Nbres_de_places_et_heures_de_stationnement','Nbre_de_site-label','Nbres_de_places_et_heures_de_stationnement-label']
+ 
+function removeAllLayersAndSources() {
+    var allSources = map.getStyle().sources;
+    console.log(allSources)
+    // Supprimer chaque couche
+    myLayers.forEach(function (layerName) {
+        if (map.getLayer(layerName)) {
+            map.removeLayer(layerName);
+        }
+    })
+ 
+    // Supprimer chaque source
+    myLayers.forEach(function (layerName) {
+        if (allSources.hasOwnProperty(`${layerName}-source`)) {
+            map.removeSource(`${layerName}-source`);
+        }
+    });
+}
+ 
+document
+    .getElementById('resetMap')
+    .addEventListener('click', removeAllLayersAndSources);
